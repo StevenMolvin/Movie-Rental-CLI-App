e07e81f98bc45733c149bc907a4806571bfeeb33
StevenMo <stevenmolvin@gmail.com>
2023-06-07 16:12:08 +0300

    Create database engine.

---
+++ models.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
+
+if __name__ == '__main__':
+    # Create the database engine
+    engine = create_engine('sqlite:///movie_rental.db')
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
