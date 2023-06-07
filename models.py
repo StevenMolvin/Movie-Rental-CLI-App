f1a6d0cab84bcf4c1cb4bdb544282997e46df02c
StevenMo <stevenmolvin@gmail.com>
2023-06-07 22:10:43 +0300

    Create rentals table using alembic

--- models.py
+++ models.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python3
-
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
 
-if __name__ == '__main__':
-    # Create the database engine
-    engine = create_engine('sqlite:///movie_rental.db')
-    Base = declarative_base()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
+# Create the database engine
+engine = create_engine('sqlite:///movie_rental.db')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
 # Define the Movie class
 class Movie(Base):
     __tablename__ = 'movies'
@@ -20,36 +17,27 @@ class Movie(Base):
     length = Column(String)
     release_date = Column(DateTime)
     rentals = relationship('Rental', back_populates='movie')
-    
+
 # Define the Customer class
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     rentals = relationship('Rental', back_populates='customer')
-    
-# Define the Rental class
-class Rental(Base):
-    __tablename__ = 'rentals'
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('customers.id'))
-    movie_id = Column(Integer, ForeignKey('movies.id'))
-    rental_date = Column(DateTime, default=datetime.now)
-    return_date = Column(DateTime)
-    movie = relationship('Movie', back_populates='rentals')
-    customer = relationship('Customer', back_populates='rentals')
-    
+
 # Create the tables in the database
 Base.metadata.create_all(engine)
+
 # Welcome menu
 def display_menu():
-    print("==== Welcome Mo's Movie Rental Database! ====")
+    print("==== Welcome Mo's Movie Rental ====")
     print("1. Rent a Movie")
     print("2. Return a Movie")
     print("3. List All Movies")
     print("4. List All Customers")
     print("5. Exit")
     
-    
-    
-    
+# Rent Movie
+def rent_movie():
+    title = input("Enter the movie title: ")
+
