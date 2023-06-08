d7208e52c269e32c9a5901eedc30ecf2c500aa7d
StevenMo <stevenmolvin@gmail.com>
2023-06-08 09:08:42 +0300

    Feat:Enable persistance on the rentals table such that when a rented movie is returned, its record is ommited from the rentals table.

--- movies.py
+++ movies.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, Foreign
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
+import click
+import fire
 
 
 # Create the database engine
@@ -15,8 +17,7 @@ class Movie(Base):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key=True)
     title = Column(String)
-    length = Column(String)
-    release_date = Column(DateTime)
+    price = Column(Integer)
     
     rentals = relationship('Rental', back_populates='movie')
 
@@ -45,18 +46,20 @@ Base.metadata.create_all(engine)
 
 # Welcome menu
 def display_menu():
-    print("==== Welcome Mo's Movie Rental ====")
+    print("==== Welcome to Mo's Movie Rentals ====")
     print("1. Rent a Movie")
     print("2. Return a Movie")
-    print("3. List All Movies")
+    print("3. List All Rented Movies")
     print("4. List All Customers")
     print("5. Exit")
     
 # Rent Movie
 def rent_movie(session):
     title = input("Enter the movie title: ")
+    price = input("Costs Ksh.  ")
     
-    movie = Movie(title= title)
+    
+    movie = Movie(title= title, price= price)
     session.add(movie)
     session.commit()
     
@@ -67,14 +70,37 @@ def rent_movie(session):
         customer = Customer(name=customer_name)
         session.add(customer)
         session.commit()
-        
-    rental = Rental(rental_ID= Rental.id, movie= movie, customer= customer)
+    
+    rental_date_str = input("Enter the rental date (DD-MM-YYYY HH:MM): ")
+    return_date_str = input("Enter the return date (DD-MM-YYYY HH:MM): ")
+    
+    try:
+        rental_date = datetime.strptime(rental_date_str, "%d-%m-%Y %H:%M")
+        return_date = datetime.strptime(return_date_str, "%d-%m-%Y %H:%M")
+    except ValueError:
+        print("Invalid date format. Please enter the dates in the format 'DD-MM-YYYY HH:MM'.")
+        return
+    
+    if return_date <= rental_date:
+        print("Return date must be after the rental date.")
+        return
+    
+    rental = Rental(movie= movie, customer= customer, rental_date= rental_date, return_date= return_date)
     session.add(rental)
     session.commit()
     
     rental_id = rental.id
+    
+    print("")
+    print("Customer name:", customer_name)
+    print("")
+    print("Movie:", title)
+    print("")
+    print("Rental ID:", rental_id)
+    print("")
     print("Movie rented successfully!")
-    print("Rental ID", rental_id)
+    print("")
+    print("")
     
 # Return Movie
 def return_movie(session):
@@ -82,29 +108,50 @@ def return_movie(session):
     rental = session.query(Rental).get(rental_id)
     
     if rental is None:
-        print("Invalid rental ID!")
+        print("Invalid ID!")
         return
     
     rental.return_date = datetime.now()
     session.commit()
     
+    print("")
     print("Movie returned successfully!")
+    print("")
+    print("")
+    
+    def delete_rental(rental_id):
+        rental = session.query(Rental).filter(Rental.id == rental_id).first()
+    
+        if rental:
+            session.delete(rental)
+            session.commit()
+            print("Rental deleted successfully!")
+        else:
+            print("Rental not found!")
+            print("")
+    delete_rental(rental_id)
     
 # List All Movies
-def list_movies(session):
+def list_rented_movies(session):
     movies = session.query(Movie).all()
-        
-    print("==== Movies ====")
+    
+    print("")    
+    print("============== RENTED MOVIES ==============")
     for movie in movies:
-        print(f"ID: {movie.id}, Title: {movie.title}, Director: {movie.director}")
+        print(f"ID: {movie.id}, Title: {movie.title}, Price: {movie.price}")
+        print("")
+        
             
 # List All Customers
 def list_customers(session):
     customers = session.query(Customer).all()
-        
-    print("==== Customer List ====")
+    
+    print("")
+    print("====== CUSTOMERS LIST ======")
     for customer in customers:
         print(f"ID: {customer.id}, Name: {customer.name}")
+        print("")
+        
             
 # Main program loop
 while True:
@@ -112,13 +159,13 @@ while True:
     choice = input("Enter you choice (1-5): ")
     
     if choice == '1':
-        rent_movie()
+        rent_movie(session)
     elif choice == '2':
-        return_movie()
+        return_movie(session)
     elif choice == '3':
-        list_movies()
+        list_rented_movies(session)
     elif choice == '4':
-        list_customers()
+        list_customers(session)
     elif choice == '5':
         break
     else:
